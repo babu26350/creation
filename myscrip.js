0423e95a3240920837b4c532b7817b44d2211e71
babu26350 <138372013+babu26350@users.noreply.github.com>
2025-06-14 22:58:49 +0530

    Update myscrip.js
    Added

--- myscrip.js
+++ myscrip.js
@@ -1,10 +1,7 @@
 // style-script.js
 
 const demo = document.getElementById('demo');
-demo.onclick = function(){
 
-  alert("clicked")
-}
 if (demo) {
   demo.textContent = 'This content and style came from GitHub script!';
   demo.style.color = 'green';
@@ -12,4 +9,8 @@ if (demo) {
   demo.style.border = '2px dashed red';
   demo.style.padding = '10px';
   demo.style.backgroundColor = '#f0f0f0';
+}
+function myFun(){
+alert("clicked")
+  
 }
