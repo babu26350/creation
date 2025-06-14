831c337fc652b929ebcf7be963b2b8f959a02e3e
babu26350 <138372013+babu26350@users.noreply.github.com>
2025-06-14 22:46:00 +0530

    Create myscrip.js
    Done

---
+++ myscrip.js
@@ -0,0 +1,12 @@
+// style-script.js
+
+const demo = document.getElementById('demo');
+
+if (demo) {
+  demo.textContent = 'This content and style came from GitHub script!';
+  demo.style.color = 'green';
+  demo.style.fontSize = '24px';
+  demo.style.border = '2px dashed red';
+  demo.style.padding = '10px';
+  demo.style.backgroundColor = '#f0f0f0';
+}
